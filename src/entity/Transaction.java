28658853d13013a8d4d4010777984e7259d99ac2
Lalaina0904 <lalainap19.aps2b@gmail.com>
2023-12-02 18:07:13 +0300

    implement entity 'transaction'

---
+++ src/entity/Transaction.java
@@ -0,0 +1,14 @@
+package entity;
+
+public class Transaction {
+    private int id;
+    private String type;
+    private Compte compte;
+    private float montant;
+    private String devise;
+    private String categorie;
+    private String etiquettes;
+    private String date;
+    private String hour;
+
+}
