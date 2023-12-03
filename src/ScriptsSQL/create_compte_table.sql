20c138c34ee9c2ef609980ba0ef660371fe49058
Marc Herilala <hei.marc.3@gmail.com>
2023-12-03 21:10:33 +0300

    migration script for table creation

--- src/ScriptsSQL/create_compte_table.sql
+++ src/ScriptsSQL/create_compte_table.sql
@@ -1,7 +1,11 @@
 CREATE  TABLE  IF NOT EXISTS  compte (
-    int id  NOT NULL AUTO_INCREMENT,
-    varchar(255) nomDuCompte  NOT NULL,
-    varchar(255) typeDeCompte  NOT NULL,
-    float montantDeDepart  NOT NULL,
-    varchar(255) devise  NOT NULL references devise(id_devise);
-)
+    id serial not null PRIMARY KEY,
+    nom_du_compte varchar(255) NOT NULL,
+    type_de_compte varchar(255) NOT NULL,
+    montant_de_depart float NOT NULL,
+    devise int NOT NULL references devise(id_devise)
+);
+INSERT INTO compte(nom_du_compte,type_de_compte,montant_de_depart,devise) values
+('test','espece',200,1),
+('test2','Assurance',100,1),
+('test3','Investissement',1000,2);
