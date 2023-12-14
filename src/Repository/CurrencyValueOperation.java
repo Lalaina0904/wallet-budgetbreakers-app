37915a6dc5c4070404432cc48d963410ddaa0ebc
Marc Herilala <hei.marc.3@gmail.com>
2023-12-14 13:45:01 +0300

    calculate average,minimumn,maximum and median

--- src/Repository/CurrencyValueOperation.java
+++ src/Repository/CurrencyValueOperation.java
@@ -4,9 +4,11 @@ import entity.Account;
 import entity.CurrencyValue;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Currency;
+import java.util.Date;
 import java.util.List;
 
 public class CurrencyValueOperation {
@@ -66,5 +68,32 @@ public class CurrencyValueOperation {
             e.printStackTrace();
         }
         return currencyValues;
+    }
+    public List<CurrencyValue> groupByDate(LocalDate date){
+        String sql = "select * from currency_value where DATE_TRUNC('day', timestamp_column) = DATE_TRUNC('day', ?)";
+        Timestamp timestamp = Timestamp.valueOf(date.atStartOfDay());
+        List<CurrencyValue> currencyValuesByDate=new ArrayList<>();
+        try {
+            PreparedStatement preparedStatement=connection.prepareStatement(sql);
+            preparedStatement.setTimestamp(1,timestamp);
+            ResultSet resultSet= preparedStatement.executeQuery();
+            while (resultSet.next()){
+               currencyValuesByDate.add(
+                       new CurrencyValue(
+
+                               resultSet.getInt(idCurrencyValueColumn),
+                               resultSet.getInt(idCurrencySourceColumn),
+                               resultSet.getInt(idCurrencyDestinationColumn),
+                               resultSet.getDouble(idCurrencyValueColumn),
+                               resultSet.getTimestamp(dateColumn).toLocalDateTime()
+                       )
+               );
+
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return currencyValuesByDate;
     }
 }
